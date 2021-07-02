90062da1d970edd8b33069cb468df55df8fa212b
Tyler Michael <tylermichael@users.noreply.github.com>
2021-07-01 23:02:17 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew install --cask dotnet-sdk
+brew install --cask jetbrains-toolbox
+brew install --cask iterm2
+brew install --cask github
+
+brew install node
+brew install docker
+brew install docker-compose
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
