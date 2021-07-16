a83fd827e60f2d587e4ede5ae49514d7891cc365
Tyler Michael <tmich214@gmail.com>
2021-07-16 15:16:05 -0400

    install some other stuff

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 
 brew tap homebrew/cask-versions
 
+brew install --cask microsoft-edge
+brew install --cask istat-menus
+brew install --cask shiftit
+brew install --cask rectangle
+brew install --cask synergy
 brew install --cask jetbrains-toolbox
 brew install --cask iterm2
 brew install --cask vscode
