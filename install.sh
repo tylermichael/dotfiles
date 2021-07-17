f7bcce9e3f4263cccda32fbb86576dcb8b45eca0
Tyler Michael <tylermichael@users.noreply.github.com>
2021-07-17 00:04:23 -0400

    rearrange and more

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew tap homebrew/cask-versions
 
 brew install --cask microsoft-edge
@@ -11,12 +16,10 @@ brew install --cask rectangle
 brew install --cask synergy
 brew install --cask jetbrains-toolbox
 brew install --cask iterm2
-brew install --cask vscode
+brew install --cask visual-studio-code
+brew install --cask 1password
 brew install --cask github
 
 brew install node
 brew install docker
 brew install docker-compose
-
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
